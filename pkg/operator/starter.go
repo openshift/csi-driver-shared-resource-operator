a0bfa6fe26a7c9bb19c4a2f50ed7bb0a12f4f05e
Corey Daley <cdaley@redhat.com>
2021-10-21 13:03:23 -0400

    updating Dockerfile to copy needed CRDs to assets folder for install

--- pkg/operator/starter.go
+++ pkg/operator/starter.go
@@ -5,10 +5,15 @@ import (
 	"fmt"
 	"time"
 
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/dynamic"
 	kubeclient "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/klog/v2"
+
+	"github.com/ghodss/yaml"
 
 	opv1 "github.com/openshift/api/operator/v1"
 	configclient "github.com/openshift/client-go/config/clientset/versioned"
@@ -52,6 +57,9 @@ func RunOperator(ctx context.Context, controllerConfig *controllercmd.Controller
 		return err
 	}
 
+	// Create apiextensions clientset for creating the CRDs
+	apiextensionsClient := apiextensionsclient.NewForConfigOrDie(controllerConfig.KubeConfig)
+
 	// Create GenericOperatorclient. This is used by the library-go controllers created down below
 	gvr := opv1.SchemeGroupVersion.WithResource("clustercsidrivers")
 	operatorClient, dynamicInformers, err := goc.NewClusterScopedOperatorClientWithConfigName(controllerConfig.KubeConfig, gvr, string(opv1.SharedResourcesCSIDriver))
@@ -59,9 +67,40 @@ func RunOperator(ctx context.Context, controllerConfig *controllercmd.Controller
 		return err
 	}
 
+	klog.V(5).Info("Generating dynamicClient")
 	dynamicClient, err := dynamic.NewForConfig(controllerConfig.KubeConfig)
 	if err != nil {
 		return err
+	}
+
+	crds := []string{"0000_10_sharedsecret.crd.yaml", "0000_10_sharedconfigmap.crd.yaml"}
+	for _, crd := range crds {
+		data, err := generated.Asset(crd)
+		if err != nil {
+			return fmt.Errorf("error occurred reading file %q: %s", crd, err)
+		}
+
+		var customResourceDefinition apiextensionsv1.CustomResourceDefinition
+		if err := yaml.Unmarshal(data, &customResourceDefinition); err != nil {
+			return fmt.Errorf("error occurred unmarshalling file %q into object: %s", crd, err)
+		}
+
+		foundItems, err := apiextensionsClient.ApiextensionsV1().CustomResourceDefinitions().List(ctx, metav1.ListOptions{
+			FieldSelector: fmt.Sprintf("metadata.name=%s", customResourceDefinition.Name),
+		})
+		if err != nil {
+			return fmt.Errorf("unexpected error occurred listing CustomResourceDefinitions: %s", err)
+		}
+
+		if len(foundItems.Items) > 0 {
+			klog.Infof("CustomResourceDefinition %q already exists, skipping creation.", customResourceDefinition.Name)
+		} else {
+
+			if _, err := apiextensionsClient.ApiextensionsV1().CustomResourceDefinitions().Create(ctx, &customResourceDefinition, metav1.CreateOptions{}); err != nil {
+				return fmt.Errorf("error occurred creating CustomResourceDefinition: %s", err)
+			}
+			klog.Infof("Successfully created CustomResourceDefinition %q.", customResourceDefinition.Name)
+		}
 	}
 
 	csiControllerSet := csicontrollerset.NewCSIControllerSet(

cd92ed8799dbe08ead2f1dfa6088333e1fd5783a
David Eads <deads@redhat.com>
2022-05-23 16:12:31 -0400

    update openshift/api to master to pick up status subresource

--- vendor/github.com/openshift/client-go/config/clientset/versioned/typed/config/v1/config_client.go
+++ vendor/github.com/openshift/client-go/config/clientset/versioned/typed/config/v1/config_client.go
@@ -3,6 +3,8 @@
 package v1
 
 import (
+	"net/http"
+
 	v1 "github.com/openshift/api/config/v1"
 	"github.com/openshift/client-go/config/clientset/versioned/scheme"
 	rest "k8s.io/client-go/rest"
@@ -20,9 +22,12 @@ type ConfigV1Interface interface {
 	FeatureGatesGetter
 	ImagesGetter
 	ImageContentPoliciesGetter
+	ImageDigestMirrorSetsGetter
+	ImageTagMirrorSetsGetter
 	InfrastructuresGetter
 	IngressesGetter
 	NetworksGetter
+	NodesGetter
 	OAuthsGetter
 	OperatorHubsGetter
 	ProjectsGetter
@@ -75,6 +80,14 @@ func (c *ConfigV1Client) ImageContentPolicies() ImageContentPolicyInterface {
 	return newImageContentPolicies(c)
 }
 
+func (c *ConfigV1Client) ImageDigestMirrorSets() ImageDigestMirrorSetInterface {
+	return newImageDigestMirrorSets(c)
+}
+
+func (c *ConfigV1Client) ImageTagMirrorSets() ImageTagMirrorSetInterface {
+	return newImageTagMirrorSets(c)
+}
+
 func (c *ConfigV1Client) Infrastructures() InfrastructureInterface {
 	return newInfrastructures(c)
 }
@@ -85,6 +98,10 @@ func (c *ConfigV1Client) Ingresses() IngressInterface {
 
 func (c *ConfigV1Client) Networks() NetworkInterface {
 	return newNetworks(c)
+}
+
+func (c *ConfigV1Client) Nodes() NodeInterface {
+	return newNodes(c)
 }
 
 func (c *ConfigV1Client) OAuths() OAuthInterface {
@@ -108,12 +125,28 @@ func (c *ConfigV1Client) Schedulers() SchedulerInterface {
 }
 
 // NewForConfig creates a new ConfigV1Client for the given config.
+// NewForConfig is equivalent to NewForConfigAndClient(c, httpClient),
+// where httpClient was generated with rest.HTTPClientFor(c).
 func NewForConfig(c *rest.Config) (*ConfigV1Client, error) {
 	config := *c
 	if err := setConfigDefaults(&config); err != nil {
 		return nil, err
 	}
-	client, err := rest.RESTClientFor(&config)
+	httpClient, err := rest.HTTPClientFor(&config)
+	if err != nil {
+		return nil, err
+	}
+	return NewForConfigAndClient(&config, httpClient)
+}
+
+// NewForConfigAndClient creates a new ConfigV1Client for the given config and http client.
+// Note the http client provided takes precedence over the configured transport values.
+func NewForConfigAndClient(c *rest.Config, h *http.Client) (*ConfigV1Client, error) {
+	config := *c
+	if err := setConfigDefaults(&config); err != nil {
+		return nil, err
+	}
+	client, err := rest.RESTClientForConfigAndClient(&config, h)
 	if err != nil {
 		return nil, err
 	}
